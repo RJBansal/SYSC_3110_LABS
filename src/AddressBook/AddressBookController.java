a7d54764ca578f570c6ef83e67334e097e894bd4
Jobin <Jobin@192.168.0.10>
2017-12-12 19:26:53 -0500

    Commented

--- src/AddressBook/AddressBookController.java
+++ src/AddressBook/AddressBookController.java
@@ -1,14 +1,17 @@
 package AddressBook;
 import java.awt.event.*;
 
+/**
+ * MVC Design pattern should technically use this class to perform all the actions.
+ * But my view kinda does it.
+ * @author Jobin Mathew
+ *
+ */
 public class AddressBookController implements ActionListener{
 
 	@Override
 	
 	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-		
-		
 	}
 
 }
